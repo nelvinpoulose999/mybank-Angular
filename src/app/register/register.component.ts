f09b6d2c079eea72a30bfcedafb8af53a6bea114
nelvinpoulose999 <nelvinpoulose999@gmail.com>
2021-06-06 20:02:03 +0530

    validations

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -8,28 +9,40 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  uname=''
-  accno=""
-  pswd=""
-
-  constructor(private dataService:DataService,private router:Router) { }
+  // uname=''
+  // accno=""
+  // pswd=""
+  registerForm = this.fb.group({
+    uname: ['', [Validators.required, Validators.pattern('[a-zA-Z]*')]],
+    accno: ["", [Validators.required, Validators.pattern('[0-9]*')]],
+    pswd: ["", [Validators.required, Validators.pattern('[a-zA-Z0-9]*')]]
+  })
+  constructor(private dataService: DataService, private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
-register(){
-  // alert('Register Clicked');
-  var accno=this.accno
-  var uname=this.uname
-  var pswd=this.pswd
-  const result=this.dataService.register(uname,accno,pswd)
+  register() {
+    // alert('Register Clicked');
 
-  if (result){
-    alert('Successfully Registered');
-    this.router.navigateByUrl('');
-  }
-  else{
-    alert('Account already exist Please Login')
-  }
+    var accno = this.registerForm.value.accno
+    var uname = this.registerForm.value.uname
+    var pswd = this.registerForm.value.pswd
+    // console.log(uname,accno,pswd);
+    if (this.registerForm.valid) {
+      const result = this.dataService.register(uname, accno, pswd)
 
-}
+      if (result) {
+        alert('Successfully Registered');
+        this.router.navigateByUrl('');
+      }
+      else {
+        alert('Account already exist Please Login')
+      }
+
+    }
+    else{
+      alert('Form Invalid')
+    }
+
+  }
 }
